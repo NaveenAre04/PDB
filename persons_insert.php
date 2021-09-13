0b308ef7ad8463aceccb311f6bfc5e1ccf9c80d7
NaveenAre04 <90562385+NaveenAre04@users.noreply.github.com>
2021-09-12 22:35:01 -0500

    Update persons_insert.php

--- persons_insert.php
+++ persons_insert.php
@@ -5,11 +5,8 @@ require_once 'config.php';
 ?>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <!-- Popper JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://kit.fontawesome.com/2e4c9e68b5.js" crossorigin="anonymous"></script>
     <title>Page Title</title>
